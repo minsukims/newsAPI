f5da3d0da85292246db42afd21fe2c31776ad3ad
Minsu Kim <thgodakfgod1@naver.com>
2026-02-24 23:08:55 +0900

    newsAPI first project

---
+++ main.js
@@ -0,0 +1,37 @@
+const API_KEY = `878a28e6216b4704aa784141272eb9f8`;
+let newsList = [];
+const getLatestNews = async () => {
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`,
+  );
+  console.log("uuu", url);
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+
+  console.log("dddd", newsList);
+  render();
+};
+const render = () => {
+  let newsHTML = ``;
+  newsHTML = newsList.map(
+    (news) => `
+    <div class="row news">
+          <div class="col-lg-4">
+            <img
+              class="news-img-size"
+             src=${news.urlToImage}
+            />
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>
+              ${news.description}
+            </p>
+            <div>${news.source.name} * ${news.publishedAt}</div>
+          </div>
+        </div>`,
+  );
+  document.querySelector("#news-board").innerHTML = newsHTML.join("");
+};
+getLatestNews();
